36bf559557076ff34712577db1afb9694908945f
Louis Faucon <lpfaucon@gmail.com>
2021-02-21 23:17:26 +0100

    Add rate later option on right click

--- background.js
+++ background.js
@@ -1,13 +1,29 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code:
-        'var videoId = (new URL(document.location)).searchParams.get("v"); \
-        if(!videoId) { \
-            alert("this extension can only be used from a Youtube video") \
-        } else { \
-            window.open(`https://dev.tournesol.app/expert/${videoId}/...`) \
-        };',
-    });
-  });
+  var videoId = new URL(tab.url).searchParams.get('v');
+  if (!videoId) {
+    alert('This extension can only be used from a Youtube video', 'ok');
+  } else {
+    open(`https://tournesol.app/expert/${videoId}/...`);
+  }
+});
+
+chrome.contextMenus.removeAll(function () {
+  chrome.contextMenus.create(
+    {
+      id: 'tournesol_add_rate_later',
+      title: 'Rate later on Tournesol',
+      contexts: ['link'],
+    },
+    function () {
+      chrome.contextMenus.onClicked.addListener(function (e, tab) {
+        var videoId = new URL(e.linkUrl).searchParams.get('v');
+        if (!videoId) {
+          alert('This must be used on a link to a youtube video', 'ok');
+        } else {
+          open(`https://tournesol.app/rate_later_add/${videoId}`, '_blank');
+          chrome.tabs.update(tab.id, { active: true });
+        }
+      });
+    },
+  );
 });

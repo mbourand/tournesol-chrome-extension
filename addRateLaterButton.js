58a8923c250778b06835f849e54172f9d65beb71
mbourand <mbourand@student.42.fr>
2021-04-22 20:17:39 +0200

    Replace double quotes by single quotes

--- addRateLaterButton.js
+++ addRateLaterButton.js
@@ -3,7 +3,7 @@
 // This part is called on connection for the first time on youtube.com/*
 /* ********************************************************************* */
 
-document.addEventListener("yt-navigate-finish", process);
+document.addEventListener('yt-navigate-finish', process);
 
 if (document.body) process();
 else document.addEventListener('DOMContentLoaded', process);
@@ -33,49 +33,49 @@ function process()
     ** Some ids on video pages are duplicated, so I take the first non-duplicated id and search in its childs the correct div to add the button
     ** Note: using .children[index] when child has no id
     */
-    if (!document.getElementById("menu-container") ||
-      !document.getElementById("menu-container").children.item("menu") ||
-      !document.getElementById("menu-container").children.item("menu").children[0] ||
-      !document.getElementById("menu-container").children.item("menu").children[0].children.item("top-level-buttons"))
+    if (!document.getElementById('menu-container') ||
+      !document.getElementById('menu-container').children.item('menu') ||
+      !document.getElementById('menu-container').children.item('menu').children[0] ||
+      !document.getElementById('menu-container').children.item('menu').children[0].children.item('top-level-buttons'))
       return;
 
 
-    console.log("Adding rate later button");
+    console.log('Adding rate later button');
     window.clearInterval(timer);
 
 
     // Create Button
-    var rateNowButton = document.createElement("button");
-    rateNowButton.setAttribute("id", "tournesol-rate-button");
-	rateNowButton.setAttribute("onclick", "window.open('https://tournesol.app/rate_later_add/" + videoId + "', '_blank')");
+    var rateNowButton = document.createElement('button');
+    rateNowButton.setAttribute('id', 'tournesol-rate-button');
+	rateNowButton.setAttribute('onclick', "window.open('https://tournesol.app/rate_later_add/" + videoId + "', '_blank')");
 
 
     // Image td for better vertical alignment
-    var img_td = document.createElement("td");
-    img_td.setAttribute("valign", "middle");
+    var img_td = document.createElement('td');
+    img_td.setAttribute('valign', 'middle');
 
 
     // Image
-    var image = document.createElement("img");
-    image.setAttribute("id", "tournesol-button-image");
-    image.setAttribute("src", chrome.extension.getURL("rate_now_icon.png"));
-    image.setAttribute("width", "20");
+    var image = document.createElement('img');
+    image.setAttribute('id', 'tournesol-button-image');
+    image.setAttribute('src', chrome.extension.getURL('rate_now_icon.png'));
+    image.setAttribute('width', '20');
     img_td.append(image);
     rateNowButton.append(img_td);
 
 
     // Text td for better vertical alignment
-    var text_td = document.createElement("td");
-    text_td.setAttribute("valign", "middle");
+    var text_td = document.createElement('td');
+    text_td.setAttribute('valign', 'middle');
 
 
     // Text
-    text_td.append(document.createTextNode("Rate Later"));
+    text_td.append(document.createTextNode('Rate Later'));
     rateNowButton.append(text_td);
 
 
     // Insert after like and dislike buttons
-    var div = document.getElementById("menu-container").children.item("menu").children[0].children.item("top-level-buttons");
+    var div = document.getElementById('menu-container').children.item('menu').children[0].children.item('top-level-buttons');
     div.insertBefore(rateNowButton, div.children[2]);
   }
 }

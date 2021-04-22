e9242d607d5d844ac7f425ce365efa2ffae30ad2
mbourand <mbourand@student.42.fr>
2021-04-22 06:24:39 +0200

    Time interval to ensure button generates correctly on refresh

--- addRateNowButton.js
+++ addRateNowButton.js
@@ -1,44 +1,55 @@
-console.log("Adding rate now button");
+// Youtube doesnt fully load a page, so content script doesn't lauch correctly without this event
+// To wait for all needed elements to exist
+var timer = window.setInterval(createButtonIsReady, 300);
 
-// Create Button
-var rateNowButton = document.createElement("button");
-rateNowButton.setAttribute("id", "tournesol-rate-button");
+function createButtonIsReady() {
 
-// Set redirect URL
-var videoId = new URL(location.href).searchParams.get('v');
-rateNowButton.setAttribute("onclick", "window.open('https://tournesol.app/rate/" + videoId + "/...', '_blank')");
+  if (!document.getElementById("menu-container") ||
+    !document.getElementById("menu-container").children.item("menu") ||
+    !document.getElementById("menu-container").children.item("menu").children[0] ||
+    !document.getElementById("menu-container").children.item("menu").children[0].children.item("top-level-buttons")) {
+    return;
+  }
 
-// Image td for better vertical alignment
-var img_td = document.createElement("td");
-img_td.setAttribute("valign", "middle");
+  console.log("Adding rate now button");
+  window.clearInterval(timer);
 
-// Image
-var image = document.createElement("img");
-image.setAttribute("id", "tournesol-button-image");
-image.setAttribute("src", chrome.extension.getURL("rate_now_icon.png"));
-image.setAttribute("width", "20");
-img_td.append(image);
+  // Create Button
+  var rateNowButton = document.createElement("button");
+  rateNowButton.setAttribute("id", "tournesol-rate-button");
 
-rateNowButton.append(img_td);
+  // Set redirect URL
+  var videoId = new URL(location.href).searchParams.get('v');
+  rateNowButton.setAttribute("onclick", "window.open('https://tournesol.app/rate/" + videoId + "/...', '_blank')");
 
-// Text td for better vertical alignment
-var text_td = document.createElement("td");
-text_td.setAttribute("valign", "middle");
+  // Image td for better vertical alignment
+  var img_td = document.createElement("td");
+  img_td.setAttribute("valign", "middle");
 
-// Text
-var text = document.createTextNode("Rate Now");
-text_td.append(text);
+  // Image
+  var image = document.createElement("img");
+  image.setAttribute("id", "tournesol-button-image");
+  image.setAttribute("src", chrome.extension.getURL("rate_now_icon.png"));
+  image.setAttribute("width", "20");
+  img_td.append(image);
+  rateNowButton.append(img_td);
 
+  // Text td for better vertical alignment
+  var text_td = document.createElement("td");
+  text_td.setAttribute("valign", "middle");
 
-rateNowButton.append(text_td);
+  // Text
+  var text = document.createTextNode("Rate Now");
+  text_td.append(text);
+  rateNowButton.append(text_td);
 
-/*
-** Some ids on video pages are duplicated, so i take the first non-duplicated id
-** and search in its childs the correct div to add the button
-** Note: using [index] when child has no id
-*/
+  /*
+  ** Some ids on video pages are duplicated, so i take the first non-duplicated id
+  ** and search in its childs the correct div to add the button
+  ** Note: using [index] when child has no id
+  */
+  var div = document.getElementById("menu-container").children.item("menu").children[0].children.item("top-level-buttons");
 
-var div = document.getElementById("menu-container").children.item("menu").children[0].children.item("top-level-buttons");
-
-// Insert after like and dislike buttons
-div.insertBefore(rateNowButton, div.children[2]);
+  // Insert after like and dislike buttons
+  div.insertBefore(rateNowButton, div.children[2]);
+}
